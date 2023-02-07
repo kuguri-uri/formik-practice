34c0712299c584a28b8ebedff12a21b5b2416105
Vera <mokeeva.vk@phystech.edu>
2023-02-07 15:31:08 +0300

    form validation (lil bug)

--- src/Form.js
+++ src/Form.js
@@ -1,31 +1,73 @@
+import { useFormik } from "formik";
 
+const validate = values => {
+    const errors = {};
+
+    if (!values.name) {
+        errors.name = "reqired name"
+    } else if (values.name.length < 2) {
+        errors.name = "at least 2 letters"
+    }
+
+    if (!values.email) {
+        errors.email = "reqired name"
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = "invalid email address"
+    } 
+
+    return errors;
+}
 
 const Form = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            amount: 0,
+            currency: '',
+            text: '',
+            terms: false
+        },
+        validate,
+        onSubmit: values => console.log(JSON.stringify(values, null, 2))
+    })
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={formik.handleSubmit}>
             <h2>Отправить пожертвование</h2>
             <label htmlFor="name">Ваше имя</label>
             <input
                 id="name"
                 name="name"
                 type="text"
+                value={formik.values.name}
+                onChange={formik.handleChange}
             />
+            {formik.errors.name? <div>{formik.errors.name}</div> : null}
             <label htmlFor="email">Ваша почта</label>
             <input
                 id="email"
                 name="email"
                 type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
             />
+            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
             <label htmlFor="amount">Количество</label>
             <input
                 id="amount"
                 name="amount"
                 type="number"
+                value={formik.values.amount}
+                onChange={formik.handleChange}
             />
             <label htmlFor="currency">Валюта</label>
             <select
                 id="currency"
-                name="currency">
+                name="currency"
+                value={formik.values.currency}
+                onChange={formik.handleChange}>
                     <option value="">Выберите валюту</option>
                     <option value="USD">USD</option>
                     <option value="UAH">UAH</option>
@@ -35,9 +77,15 @@ const Form = () => {
             <textarea 
                 id="text"
                 name="text"
+                value={formik.values.text}
+                onChange={formik.handleChange}
             />
             <label className="checkbox">
-                <input name="terms" type="checkbox" />
+                <input 
+                name="terms" 
+                type="checkbox"
+                value={formik.values.terms}
+                onChange={formik.handleChange} />
                 Соглашаетесь с политикой конфиденциальности?
             </label>
             <button type="submit">Отправить</button>
